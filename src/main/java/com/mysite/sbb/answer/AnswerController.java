bdb2052a0a99d92914937b8a6a74ba2fd443b293
EuniceNam <caellime@gmail.com>
2026-06-02 16:01:33 +0900

    add: 답변 입력 조건 추가

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,9 +2,12 @@ package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +22,17 @@ public class AnswerController {
     @PostMapping("/create/{id}")
     @Transactional
     public String createAnswer(
-            @PathVariable Integer id,
-            @RequestParam("content") String content
+            Model model,
+            @PathVariable("id") Integer id,
+            @Valid AnswerForm answerForm,
+            BindingResult bindingResult
     ) {
         Question question = questionService.getQuestion(id);
-
-        question.addAnswer(content);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        question.addAnswer(answerForm.getContent());
 
         return String.format("redirect:/question/detail/%s", id);
     }
